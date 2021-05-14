8a59af886547d90efd201ae4840bbbe29c72a044
Reonex <Ragdaresk@gmail.com>
2021-05-14 16:12:22 +0300

    Version 1.0.2

--- main.go
+++ main.go
@@ -7,9 +7,8 @@ import (
 
 func main() {
 	fmt.Println(".")
-	//task.Tree("E:\\My Programs", 0, false, []bool{true})
+	task.Tree("E:\\My Programs", 0, false, []bool{true})
 	array := []string{"Ubuntu", "Linux Mint", "OpenSUSE", "Debian", "Arch Linux", "Deepin OS", "Elementary OS", "Manjaro Linux", "Fedora", "Kali Linux"}
 	toFind := "Linux Mint"
 	fmt.Print("\n\nIndex: ", task.BinarySearch(array, toFind), "\n\n\n")
-	//task.Test()
 }
--- task/binarysearch.go
+++ task/binarysearch.go
@@ -7,19 +7,8 @@ import (
 )
 
 func BinarySearch(array []string, toFind string) int {
-	//fmt.Print("\n\n")
 	rand.Seed(time.Now().UnixNano())
 	sort.Strings(array)
-	/*fmt.Print("Array: [")
-	for i, elem := range array {
-		if i != len(array)-1 {
-			fmt.Print("\"", elem, "\", ")
-		} else {
-			fmt.Print("\"", elem, "\"")
-		}
-	}
-	fmt.Print("]\n")
-	fmt.Print("Text: \"" + toFind + "\"")*/
 	left := 0
 	right := len(array)
 	mid := (right + left) / 2
